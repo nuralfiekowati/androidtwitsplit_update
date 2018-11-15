6ee2c61901b3a04fff9a340b163c1730522f42e3
Nur Alfi Ekowati <nuralfiekowati@gmail.com>
2018-11-16 06:12:20 +0700

    -modify file
    Modifying main file.

--- src/main/java/com/tweeter/alfi/twitsplit/MainActivityTwit.kt
+++ src/main/java/com/tweeter/alfi/twitsplit/MainActivityTwit.kt
@@ -1,11 +1,16 @@
 package com.tweeter.alfi.twitsplit
 
 import android.R.layout.simple_list_item_1
+import android.app.SearchManager
+import android.content.Context
+import android.os.Build
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.Menu
 import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.SearchView
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,11 +19,16 @@ class MainActivityTwit : MainActivityTwitI, AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val actionBar = supportActionBar
+        actionBar!!.setDisplayShowHomeEnabled(true)
+        actionBar.setLogo(R.drawable.alientwiticon)
+        actionBar.setDisplayUseLogoEnabled(true)
     }
 
     override fun onStart() {
         super.onStart()
-        Log.e("tag", "onStart is working")
+        Log.e("TAG", "onStart is working")
     }
 
     val toSplit = SplitMessageTwit()
@@ -61,29 +71,66 @@ class MainActivityTwit : MainActivityTwitI, AppCompatActivity() {
         }
     }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+
+        Log.d("TAG", "Lalalala4 ")
+        val inflater = menuInflater
+        inflater.inflate(R.menu.menu_main, menu)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            val manager = getSystemService(Context.SEARCH_SERVICE) as SearchManager
+            val search = menu.findItem(R.id.search).actionView as SearchView
+            search.setSearchableInfo(manager.getSearchableInfo(componentName))
+
+            search.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+
+                override fun onQueryTextSubmit(s: String): Boolean {
+                    Log.d("TAG", "onQueryTextSubmit ")
+                    return false
+                }
+
+                override fun onQueryTextChange(s: String): Boolean {
+
+                    if (s == "") {
+                        Log.d("TAG", "onQueryTextChange is Empty ")
+                    } else {
+                        Log.d("TAG", "onQueryTextChange is not Empty")
+                    }
+
+                    return false
+
+                }
+
+            })
+
+        }
+
+        return true
+    }
+
     override fun onResume() {
         super.onResume()
-        Log.e("tag", "onResume is working")
+        Log.e("TAG", "onResume is working")
     }
 
     override fun onPause() {
         super.onPause()
-        Log.e("tag", "onPause is working")
+        Log.e("TAG", "onPause is working")
     }
 
     override fun onStop() {
         super.onStop()
-        Log.e("tag", "onStop is working")
+        Log.e("TAG", "onStop is working")
     }
 
     override fun onRestart() {
         super.onRestart()
-        Log.e("tag", "onRestart is working")
+        Log.e("TAG", "onRestart is working")
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.e("tag", "onDestroy is working")
+        Log.e("TAG", "onDestroy is working")
     }
 
 }
